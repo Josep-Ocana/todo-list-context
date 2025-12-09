d5147937b68818ef7049a2430ec75b4ed3890f78
Josep Ocaña <j.ocanapuigdevall@gmail.com>
2025-12-09 18:53:11 +0100

    initialState and functions in Context, prev in setTodos

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,17 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Stats from "./components/Stats";
 import Form from "./components/TodoForm";
 import TodoList from "./components/TodoList";
-import type { Todo } from "./types";
+import { useTodos } from "./context/useTodos";
 
 function App() {
-  const initialState = () => {
-    const almacenados = localStorage.getItem("todos");
-    return almacenados ? JSON.parse(almacenados) : [];
-  };
-
-  const [todos, setTodos] = useState<Todo[]>(initialState);
+  const { todos, addTodo, onDelete, onToggle, updateTodo } = useTodos();
 
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos));
   }, [todos]);
-
-  const addTodo = (tarea: string) => {
-    const todo: Todo = {
-      id: Date.now(),
-      text: tarea,
-      completed: false,
-    };
-    setTodos([...todos, todo]);
-  };
-
-  const onDelete = (id: Todo["id"]) => {
-    setTodos(todos.filter((tarea) => tarea.id !== id));
-  };
-
-  const onToggle = (id: Todo["id"]) => {
-    setTodos(
-      todos.map((todo) => {
-        if (todo.id === id) {
-          return { ...todo, completed: !todo.completed };
-        }
-        return todo;
-      })
-    );
-  };
-
-  const updateTodo = (id: Todo["id"], newText: string) => {
-    setTodos(
-      todos.map((todo) => {
-        if (todo.id === id) {
-          return { ...todo, text: newText };
-        }
-        return todo;
-      })
-    );
-  };
 
   return (
     <>
--- src/context/TodoContext.tsx
+++ src/context/TodoContext.tsx
@@ -1,17 +1,27 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import type { Todo } from "../types";
 
+// 1. TYPE
 type TodoContextType = {
   todos: Todo[];
   addTodo: (text: string) => void;
+  onDelete: (id: Todo["id"]) => void;
+  onToggle: (id: Todo["id"]) => void;
+  updateTodo: (id: number, newText: string) => void;
 };
 
+// 2. CONTEXT
 export const TodoContext = createContext<TodoContextType | null>(null);
 
-import { useState } from "react";
+// 2. INITIALSTATE
+const initialState = () => {
+  const almacenados = localStorage.getItem("todos");
+  return almacenados ? JSON.parse(almacenados) : [];
+};
 
+// 3. PROVIDER
 export function TodoProvider({ children }: { children: React.ReactNode }) {
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todos, setTodos] = useState<Todo[]>(initialState);
 
   const addTodo = (text: string) => {
     const newTodo = {
@@ -19,11 +29,39 @@ export function TodoProvider({ children }: { children: React.ReactNode }) {
       text,
       completed: false,
     };
-    setTodos([...todos, newTodo]);
+    setTodos((prev) => [...prev, newTodo]);
+  };
+
+  const onDelete = (id: Todo["id"]) => {
+    setTodos((prev) => prev.filter((tarea) => tarea.id !== id));
+  };
+
+  const onToggle = (id: Todo["id"]) => {
+    setTodos((prev) =>
+      prev.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed };
+        }
+        return todo;
+      })
+    );
+  };
+
+  const updateTodo = (id: Todo["id"], newText: string) => {
+    setTodos((prev) =>
+      prev.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, text: newText };
+        }
+        return todo;
+      })
+    );
   };
 
   return (
-    <TodoContext.Provider value={{ todos, addTodo }}>
+    <TodoContext.Provider
+      value={{ todos, addTodo, onDelete, onToggle, updateTodo }}
+    >
       {children}
     </TodoContext.Provider>
   );
