53f7cd9ab78a8263d4edb150a1a876edb492b969
Josep Ocaña <j.ocanapuigdevall@gmail.com>
2025-12-10 19:16:43 +0100

    change tarea's color in TodoList

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -10,7 +10,7 @@ const TodoList = () => {
         <p className="bg-white shadow-lg rounded-lg p-5 text-lg text-center">
           {" "}
           No tienes ninguna{" "}
-          <span className="text-sky-600 font-black">tarea</span>{" "}
+          <span className="text-cyan-700 font-black">Tarea</span>{" "}
         </p>
       ) : (
         <div className="space-y-3">
